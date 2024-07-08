587f3b5973367f65460ef2a0bbca0bdcc24f0a31
chioma-okeke <okekegeraldenne@gmail.com>
2024-07-08 10:54:22 +0100

    modified first page

--- src/pages/FirstPage.jsx
+++ src/pages/FirstPage.jsx
@@ -1,10 +1,14 @@
-import React from "react";
+// import React from "react";
 import { Link } from "react-router-dom";
 
 function FirstPage() {
     return (
-        <div>
-            <Link to="/home">FirstPage</Link>
+        <div className="min-h-screen w-full flex items-center justify-center">
+            <Link to="/home">
+                <button className="bg-gradient-to-tr from-[#db1307] to-[#ffeae4] rounded-3xl p-5 text-white font-bold transition ease-in-out hover:scale-110 hover:shadow-md hover:shadow-[#db1307] duration-500">
+                    Click me
+                </button>
+            </Link>
         </div>
     );
 }
